a782433b5747386841d80f6bcd27639615d7b9b4
ladislav-moravec <118938709+ladislav-moravec@users.noreply.github.com>
2024-04-06 16:41:31 +0200

    Update create_db.py

--- postgresql/create_db.py
+++ postgresql/create_db.py
@@ -3,33 +3,34 @@ import psycopg2
 def create_database():
     # Připojení k defaultní databázi PostgreSQL (template1)
     conn = psycopg2.connect(
-        dbname="template1",
-        user="your_username",
-        password="your_password",
-        host="your_host"
+        dbname="postgres",
+        user="postgres",
+        password="asdf",
+        host="localhost"
     )
+    conn.autocommit = True
     cur = conn.cursor()
 
     # Vytvoření nové databáze s názvem sreality
-    cur.execute("CREATE DATABASE sreality")
+    cur.execute("CREATE DATABASE master")
 
     # Uzavření spojení s template1 databází
     cur.close()
     conn.close()
 
-def create_table():
+def create_table(name_table):
     # Připojení k nově vytvořené databázi sreality
     conn = psycopg2.connect(
-        dbname="sreality",
-        user="your_username",
-        password="your_password",
-        host="your_host"
+        dbname="master",
+        user="postgres",
+        password="asdf",
+        host="localhost"
     )
     cur = conn.cursor()
 
     # Vytvoření tabulky estates, pokud ještě neexistuje
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS estates (
+    cur.execute(f"""
+    CREATE TABLE IF NOT EXISTS {name_table} (
         id SERIAL PRIMARY KEY,
         name VARCHAR,
         price INTEGER,
@@ -52,7 +53,13 @@ if __name__ == "__main__":
         print("Chyba při vytváření databáze:", e)
 
     try:
-        create_table()
-        print("Tabulka estates byla úspěšně vytvořena.")
+        create_table("sreality")
+        print("Tabulka sreality byla úspěšně vytvořena nebo již existuje.")
+    except psycopg2.Error as e:
+        print("Chyba při vytváření tabulky:", e)
+
+    try:
+        create_table("idnes_reality")
+        print("Tabulka idnes_reality byla úspěšně vytvořena nebo již existuje.")
     except psycopg2.Error as e:
         print("Chyba při vytváření tabulky:", e)
